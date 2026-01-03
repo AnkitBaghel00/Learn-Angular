03748c3b86a3426ed4b21bdc3079736457bc6c41
baghe <baghelankit602@gmail.com>
2026-01-04 01:33:34 +0530

    data Bindling

---
+++ src/app/components/data-binding/data-binding.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css',
+})
+export class DataBinding {
+  courseName: string = "Angular 20 Tutorial";
+  productPrice: number = 12500;
+  maxLength: number = 5;
+  minChar: number = 3;
+  inputType: string = "checkBox"
+  myClassName: string = "myColor";
+
+
+  constructor(){
+    // this.courseName = 
+  }
+
+  showWelcomeMessage() {
+    alert("welcome to Angular 20")
+  }
+
+  changeCourseName() {
+    this.courseName = "Reactjs Tutorial"
+  }
+
+  onCityChange(){
+    alert("City Changed")
+  }
+}
