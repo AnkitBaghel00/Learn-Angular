ad9e98ab6b51a6df4ed7cffc8a3fccd69e5743e9
baghe <baghelankit602@gmail.com>
2026-01-03 20:27:50 +0530

    learned Standalone component

---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css',
+})
+export class Admin {
+ 
+  name: string = "Ankit";
+  courseName: string = "Angular 20 learning";
+  rollNo = 111;
+
+}
