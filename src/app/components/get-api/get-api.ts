4011e18f70f2dfc0e5772bd902a47d43dfbd567e
baghe <baghelankit602@gmail.com>
2026-01-06 21:51:58 +0530

    Get Api

---
+++ src/app/components/get-api/get-api.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api',
+  imports: [],
+  templateUrl: './get-api.html',
+  styleUrl: './get-api.css',
+})
+export class GetApi implements OnInit {
+
+  http = inject(HttpClient);
+  userList: any[] = [];
+  todoList: any[] = [];
+
+  ngOnInit(): void {
+    debugger;
+    this.getUsers();
+    this.getTodoItems();
+  }
+
+  getUsers() {
+    debugger;
+    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((result:any) => {
+      debugger;
+      this.userList = result;
+    })
+  }
+
+  getTodoItems() {
+    this.http.get("https://jsonplaceholder.typicode.com/todos").subscribe((response:any) => {
+      this.todoList = response;
+    })
+  }
+}
